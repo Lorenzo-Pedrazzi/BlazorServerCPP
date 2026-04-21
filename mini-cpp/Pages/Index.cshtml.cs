c9786631e00d9ea18c7bb5c4bca21f781d794db9
Vanessa <roli.vanessa@elettrondata.it>
2026-04-21 15:33:14 +0200

    Aggiunti file progetto locale

---
+++ mini-cpp/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mini_cpp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
