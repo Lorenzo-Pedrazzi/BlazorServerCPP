df91e2d63c3d939b4064a84bd90050727e00259a
Vanessa <roli.vanessa@elettrondata.it>
2026-04-23 11:08:30 +0200

    file di specifiche

--- obj/Release/net10.0/BlazorServerCPP.AssemblyInfo.cs
+++ obj/Release/net10.0/BlazorServerCPP.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorServerCPP")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d27bcc8e08037dc69e94f76f8d476b49a6b8b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorServerCPP")]
